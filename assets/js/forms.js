342ee27af557633b8c83f74e6e219728fac8186a
Juan Garcia <140569072+Jddg95@users.noreply.github.com>
2024-03-16 15:38:31 -0500

    created starter code

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,15 +1,32 @@
 const usernameInput = document.getElementById('username');
 const titleInput = document.getElementById('title');
 const contentInput = document.getElementById('content');
+const submit = document.getElementById('submit-btn')
 
-function createAndRenderBlog() {
+function createAndRenderBlog(evt) {
+    //preventing page from refreshing 
+    evt.preventDefault()
+    // creating an obj.
     const blog = {
         username: usernameInput.value.trim(),
         title: titleInput.value.trim(),
         contentInput: contentInput.value.trim()
     };
     // parsing (converting) get item whick is an object into an array
-    const blogs = JSON.parse(localStorage.getItem('blogs'))
+    // gets any previous blogs and adds them to the list, if there are no 
+    // previous blogs we make a new list with our new blog and save.
+    const blogs = JSON.parse(localStorage.getItem('blogs')) || []
+    console.log(blogs);
     blogs.push(blog)
-    localStorage.setItem(blog)
+    localStorage.setItem('blogs',JSON.stringify(blogs))
 }
+
+// function to store user input on the console. 
+function submitBlog (evt) {
+    evt.preventDefault()
+    console.log(usernameInput.value);
+    console.log(titleInput.value);
+    console.log(contentInput.value);
+}
+
+submit.addEventListener('click', createAndRenderBlog);
