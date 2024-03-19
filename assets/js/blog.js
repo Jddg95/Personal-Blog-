037dfebbc33a3abfd79c521498d62d51b0900f44
Juan Garcia <140569072+Jddg95@users.noreply.github.com>
2024-03-18 22:52:51 -0500

    finalized code

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,21 +1,32 @@
+// retrieving blogs from local storage and parsing it into an array 
 const blogs = JSON.parse(localStorage.getItem('blogs'))
+
+// getting reference to the html element that will hold the blogs 
 const blogHolder = document.getElementById('blogs')
+
 // loop through all blogs 
 blogs.forEach(element => {
-    // created elements 
-    const title = document.createElement('h2')
-    const username = document.createElement('h4')
-    const content = document.createElement('p')
-    // assigned text to elements based on blog property
-    title.textContent = element.title
-    username.textContent = element.username
-    content.textContent = element.contentInput
-    // created new div to hold blogs
+    // created elements to display blog details 
+    const title = document.createElement('h2') // heading for the blog title 
+    const username = document.createElement('h4') // subheading for username 
+    const content = document.createElement('p') // paragraph for blog content 
+
+    // assigned text content  to elements based on blog property
+    title.textContent = element.title // setting title text 
+    username.textContent = element.username // setting username text 
+    content.textContent = element.contentInput // setting content text 
+
+    // created new div to hold individual blog elements 
     const blogContainer = document.createElement('div')
-    blogContainer.appendChild(title)
-    blogContainer.appendChild(username)
-    blogContainer.appendChild(content)
-    // displays blogs on page 
+
+    // appending blog details to the blog container 
+    blogContainer.appendChild(title) // adding title 
+    blogContainer.appendChild(username) // adding username 
+    blogContainer.appendChild(content) // adding content 
+
+    // displays blogs container on the page 
     blogHolder.appendChild(blogContainer)
+
+    // loging the currrent blog element to the console 
     console.log(element)
 });
